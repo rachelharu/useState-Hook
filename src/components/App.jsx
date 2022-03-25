1b61e1fa99f07a0e3d81e1c3c2b535906d415d28
rachelharu <86934324+rachelharu@users.noreply.github.com>
2022-03-25 13:20:34 -0700

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+
+//create app that has useState hook for the counter
+//create function to increase count
+//create function to decrease count
+//set buttons for increase and decrease
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  function increase() {
+    setCount(count + 1);
+  }
+
+  function decrease() {
+    setCount(count - 1);
+  }
+
+  return (
+    <div className="container">
+      <h1>{count}</h1>
+      <button onClick={decrease}>-</button>
+      <button onClick={increase}>+</button>
+    </div>
+  );
+}
+
+export default App;
